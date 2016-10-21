e39fbdb77b62017d9d989b07009d0ec8d6807ac1
Dudás Ádám <sir.dudas.adam@gmail.com>
2016-10-22 01:53:59 +0200

    initial commit

---
+++ build-pages.js
@@ -0,0 +1,36 @@
+const fs = require('fs');
+const handlebars = require('handlebars');
+const marked = require('marked');
+const mkdirp = require('mkdirp');
+const path = require('path');
+
+const pages = require('./pages.json');
+
+handlebars.registerHelper('render', (source) => marked(fs.readFileSync(source, {encoding: 'utf8'})));
+
+const outputDir = process.env.npm_package_config_output_dir;
+const stylesheets = [];
+
+pages.forEach(({template, partials, context, target}) => {
+    Object.keys(partials).forEach((key) => {
+        handlebars.registerPartial(key, fs.readFileSync(partials[key], {encoding: 'utf8'}));
+    });
+    const outputPath = path.join(outputDir, target);
+    mkdirp.sync(path.dirname(outputPath));
+    fs.writeFileSync(outputPath, handlebars.compile(fs.readFileSync(template, {encoding: 'utf8'}))(context));
+
+    if (context.stylesheets) {
+        context.stylesheets.forEach((stylesheet) => {
+            if (stylesheets.indexOf(stylesheet) < 0) {
+                stylesheets.push(stylesheet);
+            }
+        });
+    }
+});
+
+stylesheets.forEach((stylesheet) => {
+    const inputPath = path.join('./stylesheets', stylesheet);
+    const outputPath = path.join(outputDir, 'stylesheets', stylesheet);
+    mkdirp.sync(path.dirname(outputPath));
+    fs.createReadStream(inputPath).pipe(fs.createWriteStream(outputPath));
+});
