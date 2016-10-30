030b00f10c259b9b7360c85996a44221e552099f
Dudás Ádám <sir.dudas.adam@gmail.com>
2016-10-30 21:37:47 +0100

    only copy newer files on build

--- build-pages.js
+++ build-pages.js
@@ -4,20 +4,50 @@ const marked = require('marked');
 const mkdirp = require('mkdirp');
 const path = require('path');
 
+function copy(sourcePath, destinationPath, callback) {
+    console.log("Copying from", sourcePath, "to", destinationPath);
+    mkdirp(path.dirname(destinationPath), (err) => {
+        if (err) callback(err);
+        else fs.createReadStream(sourcePath)
+            .pipe(fs.createWriteStream(destinationPath))
+            .on('error', (err) => callback(err))
+            .on('finish', () => callback());
+    });
+}
+
+function isUpToDate(sourcePath, destinationPath, callback) {
+    fs.stat(sourcePath, (srcErr, srcStat) => {
+        if (srcErr) callback(srcErr);
+        else fs.stat(destinationPath, (dstErr, dstStat) => {
+            if (dstErr && dstErr.code !== 'ENOENT') callback(dstErr);
+            else if (!dstErr && Date.parse(srcStat.mtime) < Date.parse(dstStat.mtime)) callback(null, true);
+            else callback(null, false);
+        });
+    });
+}
+
+function update(sourcePath, destinationPath, callback) {
+    isUpToDate(sourcePath, destinationPath, (err, upToDate) => {
+        if (err) callback(err);
+        else if (upToDate) callback();
+        else copy(sourcePath, destinationPath, callback); 
+    });
+}
+
 const pages = require('./pages.json');
 
 handlebars.registerHelper('render', (source) => marked(fs.readFileSync(source, {encoding: 'utf8'})));
 
-const outputDir = process.env.npm_package_config_output_dir;
+const targetRoot = process.env.npm_package_config_target_root;
 const stylesheets = [];
 
 pages.forEach(({template, partials, context, target}) => {
     Object.keys(partials).forEach((key) => {
         handlebars.registerPartial(key, fs.readFileSync(partials[key], {encoding: 'utf8'}));
     });
-    const outputPath = path.join(outputDir, target);
-    mkdirp.sync(path.dirname(outputPath));
-    fs.writeFileSync(outputPath, handlebars.compile(fs.readFileSync(template, {encoding: 'utf8'}))(context));
+    const targetPath = path.join(targetRoot, target);
+    mkdirp.sync(path.dirname(targetPath));
+    fs.writeFileSync(targetPath, handlebars.compile(fs.readFileSync(template, {encoding: 'utf8'}))(context));
 
     if (context.stylesheets) {
         context.stylesheets.forEach((stylesheet) => {
@@ -29,10 +59,9 @@ pages.forEach(({template, partials, context, target}) => {
 });
 
 stylesheets.forEach((stylesheet) => {
-    const inputPath = path.join('./resources/stylesheets', stylesheet);
-    const outputPath = path.join(outputDir, 'stylesheets', stylesheet);
-    mkdirp.sync(path.dirname(outputPath));
-    fs.createReadStream(inputPath).pipe(fs.createWriteStream(outputPath));
+    const sourcePath = path.join('./resources/stylesheets', stylesheet);
+    const destinationPath = path.join(targetRoot, 'stylesheets', stylesheet);
+    update(sourcePath, destinationPath, (err) => err && console.error(err));
 });
 
-fs.createReadStream('./resources/favicon.png').pipe(fs.createWriteStream(path.join(outputDir, 'favicon.png')));
+update('./resources/favicon.png', path.join(targetRoot, 'favicon.png'), (err) => err && console.error(err));
